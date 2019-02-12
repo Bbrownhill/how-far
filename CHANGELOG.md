2e276d4aad4e20cf41e0518d11e176f89f04c039
sorcerermjolnir <sorcerer_mjolnir@live.com>
2019-02-12 13:19:33 +0000

    update setup.py, add license and update changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,6 @@ Categories: Removed, Changed, Added, Deprecated, Fixed, Security, Nonfunctional
 ## Unreleased
 
 ### Added
+
+ - How_far click application itself.
+ - targets.yml to specify valid bodies to query
--- setup.py
+++ setup.py
@@ -1,15 +1,32 @@
 from distutils.core import setup
+from os import path
+
+
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+def get_requirements():
+    with open('requirements.txt', 'r') as file:
+        requirements = file.read().split('\n')
+    requirements.remove('')
+    return requirements
+
 
 setup(
     name='how_far',
     version='0.1.0',
+    URL='https://github.com/sorcerermjolnir/how_far/',
+    author='Robert Brownhill',
+    author_email='rbrownhill@live.co.uk',
+    license='gpl-3.0',
+    license_file='LICENSE',
+    description='Display realtime distances between planets',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
     packages=['src'],
-    install_requires=[
-        'click==7.0',
-        'astroquery==0.3.9',
-        'astropy==3.1.1',
-        'pyyaml==3.13',
-    ],
+    install_requires=get_requirements(),
     entry_points={
         'console_scripts': [
                     'how_far=src.how_far:how_far',
